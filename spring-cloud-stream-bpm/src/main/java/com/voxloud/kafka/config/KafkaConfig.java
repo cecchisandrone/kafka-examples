6adfb46abadf504aa01401917f36a042728ea302
Alessandro Dionisi <a.dionisi@voverc.com>
2019-10-23 16:49:18 +0200

    Added custom config for RetryTemplate

--- spring-cloud-stream-bpm/src/main/java/com/voxloud/kafka/config/KafkaConfig.java
+++ spring-cloud-stream-bpm/src/main/java/com/voxloud/kafka/config/KafkaConfig.java
@@ -3,11 +3,28 @@ package com.voxloud.kafka.config;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.Input;
 import org.springframework.cloud.stream.annotation.Output;
+import org.springframework.cloud.stream.annotation.StreamRetryTemplate;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.SubscribableChannel;
+import org.springframework.retry.backoff.ExponentialBackOffPolicy;
+import org.springframework.retry.policy.SimpleRetryPolicy;
+import org.springframework.retry.support.RetryTemplate;
 
 @EnableBinding({KafkaConfig.OrderCreatedBinding.class, KafkaConfig.PaymentCreatedBinding.class, KafkaConfig.ShipmentCreatedBinding.class})
 public class KafkaConfig {
+
+    // This is used to retry failed processing in @StreamListener
+    @StreamRetryTemplate
+    public RetryTemplate kafkaRetryTemplate() {
+        RetryTemplate retryTemplate = new RetryTemplate();
+        ExponentialBackOffPolicy exponentialBackOffPolicy = new ExponentialBackOffPolicy();
+        exponentialBackOffPolicy.setInitialInterval(1000L);
+        SimpleRetryPolicy retryPolicy = new SimpleRetryPolicy();
+        retryPolicy.setMaxAttempts(5);
+        retryTemplate.setRetryPolicy(retryPolicy);
+        retryTemplate.setBackOffPolicy(exponentialBackOffPolicy);
+        return retryTemplate;
+    }
 
     public interface OrderCreatedBinding {
 
