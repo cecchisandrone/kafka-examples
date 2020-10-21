cf3d18bb35fb2e671b45c43853aa78790ef4d8e1
Alessandro Dionisi <a.dionisi@voverc.com>
2020-10-21 15:24:03 +0200

    Fixes

--- spring-kafka/src/main/java/com/voxloud/kafka/consumer/MessageConsumer.java
+++ spring-kafka/src/main/java/com/voxloud/kafka/consumer/MessageConsumer.java
@@ -5,21 +5,19 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 
 @Slf4j
 public class MessageConsumer {
 
-    @KafkaListener(topicPartitions = @TopicPartition(topic = "${event.topic.name}", partitions = {"0", "1", "2"}),
-                   containerFactory = "eventKafkaListenerContainerFactory")
-    public void eventListenerPartition(BaseEvent event, @Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partition) throws InterruptedException {
+    @KafkaListener(topics = "${event.topic.name}", containerFactory = "baseEventKafkaListenerContainerFactory")
+    public void eventListenerPartition(BaseEvent event, @Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partition) {
         log.info("Received event {} on partition {}", event, partition);
     }
 
     @KafkaListener(topics = "${order.topic.name}", containerFactory = "orderKafkaListenerContainerFactory")
-    public void orderListener(ConsumerRecord<?, ?> order) {
-        log.info("Received order {}", order.value());
+    public void orderListener(ConsumerRecord<?, ?> order, @Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partition) {
+        log.info("Received order {} on partition {}", order.value(), partition);
     }
 }
